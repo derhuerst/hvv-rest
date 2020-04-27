8db740ba59bddb23d056e9261da99e005aa58039
Jannis R <mail@jannisr.de>
2020-04-27 19:45:03 +0200

    caching using cached-hafas-client

--- index.js
+++ index.js
@@ -2,13 +2,26 @@
 
 const createHafas = require('hafas-client')
 const hvvProfile = require('hafas-client/p/hvv')
-const createApi = require('hafas-rest-api')
+const {createClient: createRedis} = require('redis')
+const withCache = require('cached-hafas-client')
+const redisStore = require('cached-hafas-client/stores/redis')
 const createHealthCheck = require('hafas-client-health-check')
+const createApi = require('hafas-rest-api')
 const pkg = require('./package.json')
 
 const hamburgHarburg = '3204'
 
 const hafas = createHafas(hvvProfile, 'hvv-rest')
+const checkHafas = createHealthCheck(hafas, hamburgHarburg)
+
+const redis = createRedis()
+const checkRedis = () => new Promise((resolve, reject) => {
+	setTimeout(reject, 1000, new Error('didn\'t receive a PONG'))
+	redis.ping((err, res) => {
+		if (err) reject(err)
+		else resolve(res === 'PONG')
+	})
+})
 
 const config = {
 	hostname: process.env.HOSTNAME || 'localhost',
@@ -20,10 +33,18 @@ const config = {
 	docsLink: 'https://github.com/derhuerst/hvv-rest/blob/5/docs/readme.md',
 	logging: true,
 	aboutPage: true,
-	healthCheck: createHealthCheck(hafas, hamburgHarburg)
+	healthCheck: async () => (
+		(await checkHafas()) === true &&
+		(await checkRedis()) === true
+	),
 }
 
-const api = createApi(hafas, config, () => {})
+const client = withCache(hafas, redisStore(redis))
+const api = createApi(client, config, () => {})
+
+redis.on('error', (err) => {
+	api.locals.logger.error(err)
+})
 
 api.listen(config.port, (err) => {
 	const {logger} = api.locals
