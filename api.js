559637633c1874c942923aa04d970c27e3508ffe
Jannis R <mail@jannisr.de>
2021-11-28 13:41:09 +0100

    redis -> ioredis, cached-hafas-client@4

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const {join: pathJoin} = require('path')
 const createHafas = require('hafas-client')
 const hvvProfile = require('hafas-client/p/hvv')
 const createHealthCheck = require('hafas-client-health-check')
-const {createClient: createRedis} = require('redis')
+const Redis = require('ioredis')
 const withCache = require('cached-hafas-client')
 const redisStore = require('cached-hafas-client/stores/redis')
 const createApi = require('hafas-rest-api')
@@ -19,21 +19,16 @@ let hafas = createHafas(hvvProfile, 'hvv-rest')
 let healthCheck = createHealthCheck(hafas, hamburgHarburg)
 
 if (process.env.REDIS_URL) {
-	const redis = createRedis({
-		url: process.env.REDIS_URL,
-	})
-	redis.on('error', (err) => {
-		api.locals.logger.error(err)
-	})
+	const redis = new Redis(process.env.REDIS_URL || null)
 	hafas = withCache(hafas, redisStore(redis))
 
 	const checkHafas = healthCheck
 	const checkRedis = () => new Promise((resolve, reject) => {
 		setTimeout(reject, 1000, new Error('didn\'t receive a PONG'))
-		redis.ping((err, res) => {
-			if (err) reject(err)
-			else resolve(res === 'PONG')
-		})
+		redis.ping().then(
+			res => resolve(res === 'PONG'),
+			reject,
+		)
 	})
 	healthCheck = async () => (
 		(await checkHafas()) === true &&
